59f816d4d90457f9fc068f7bb3d71535f118966a
FabioLucena <joe04_lo@hotmail.com>
2022-02-11 16:54:22 -0300

    primeiro desafio adicionado

--- modulo4/projeto-LabECommerce/src/endpoints/getAllProducts.ts
+++ modulo4/projeto-LabECommerce/src/endpoints/getAllProducts.ts
@@ -3,20 +3,46 @@ import { connection } from "../data/connection"
 import { user } from "../types"
 
 export const getAllProducts = async (req: Request, res: Response): Promise<void> => {
-    try{
-        
-        const products = await getProducts()
+    try {
+        const order = req.query.order
+        const search = req.query.search
+        let sort: string
+        let products: any
+
+        if (!search) {
+            if (order === "asc") {
+                sort = "ORDER BY name ASC"
+                products = await getProducts(sort)
+            } else if (order === "desc") {
+                sort = "ORDER BY name DESC"
+                products = await getProducts(sort)
+            } else if (!order) {
+                sort = ""
+                products = await getProducts(sort)
+            }
+        }else{
+            if (order === "asc") {
+                sort = `WHERE name LIKE "%${search}%" ORDER BY name ASC`
+                products = await getProducts(sort)
+            } else if (order === "desc") {
+                sort = `WHERE name LIKE "%${search}%" ORDER BY name DESC`
+                products = await getProducts(sort)
+            } else if (!order) {
+                sort = `WHERE name LIKE "%${search}%"`
+                products = await getProducts(sort)
+            }
+        }
 
         res.status(200).send({ products })
 
-    }catch(error: any){
+    } catch (error: any) {
         res.send(error.message)
     }
 }
 
-export async function getProducts(): Promise<any> {
+export async function getProducts(sort: string): Promise<any> {
     const result = await connection.raw(`
-      SELECT * FROM labecommerce_products;      
+      SELECT * FROM labecommerce_products ${sort} ;      
    `)
 
     return result[0]
