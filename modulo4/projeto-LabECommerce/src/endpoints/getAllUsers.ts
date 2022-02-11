c1856dd3a3b1b48e324a62e9d61177b0ae7cddf7
FabioLucena <joe04_lo@hotmail.com>
2022-02-11 17:13:45 -0300

    segundo desafio adicionado

--- modulo4/projeto-LabECommerce/src/endpoints/getAllUsers.ts
+++ modulo4/projeto-LabECommerce/src/endpoints/getAllUsers.ts
@@ -5,7 +5,21 @@ import { user } from "../types"
 export const getAllUsers = async (req: Request, res: Response): Promise<void> => {
     try{
         
-        const users = await getUsers()
+        const user = await getUsers()
+        const allPurchases = await getPurchases()
+
+
+        const users = user.map((item: any)=>{
+            const purchases: any = allPurchases.filter((purchase: any)=>{return purchase.user_id === item.id})
+
+            return {
+                id: item.id,
+                name: item.name,
+                email: item.email,
+                password: item.password,
+                purchases: purchases
+            }
+        })
 
         res.status(200).send({ users })
 
@@ -17,6 +31,14 @@ export const getAllUsers = async (req: Request, res: Response): Promise<void> =>
 export async function getUsers(): Promise<any> {
     const result = await connection.raw(`
       SELECT * FROM labecommerce_users;      
+   `)
+
+    return result[0]
+} 
+
+ export async function getPurchases(): Promise<any> {
+    const result = await connection.raw(`
+      SELECT * FROM labecommerce_purchases;      
    `)
 
     return result[0]
