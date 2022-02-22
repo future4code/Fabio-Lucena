f7ffb1ef445fe45a1dfc533b736afc8125792505
FabioLucena <joe04_lo@hotmail.com>
2022-02-22 10:32:32 -0300

    exercicio 5 adicionado

--- modulo5/introducao-autenticacao/template-intro-autenticacao/src/endpoints/buscaUsuarioPorEmail.ts
+++ modulo5/introducao-autenticacao/template-intro-autenticacao/src/endpoints/buscaUsuarioPorEmail.ts
@@ -4,4 +4,6 @@ export const buscaUsuarioPorEmail = async(email: string): Promise<any> =>{
     const result = await connection.raw(`
     SELECT * FROM User_aula_55 WHERE email = '${email}';
     `)
+
+    return result[0]
 } 
