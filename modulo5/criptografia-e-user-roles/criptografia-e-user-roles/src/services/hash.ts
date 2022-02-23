6a03bd7ef3c22dc01ad9ff4b158b8a4ce6a72ffe
FabioLucena <joe04_lo@hotmail.com>
2022-02-23 15:24:15 -0300

    funções de criação e comparação de hash adicionadas

---
+++ modulo5/criptografia-e-user-roles/criptografia-e-user-roles/src/services/hash.ts
@@ -0,0 +1,13 @@
+import * as bcrypt from "bcryptjs";
+
+export const generateHash = async(s: string): Promise<string> =>{
+    const rounds = Number(process.env.BCRYPT_COST)
+    const salt = await bcrypt.genSalt(rounds)
+    const result = await bcrypt.hash(s, salt)
+
+    return result
+}
+
+export const compareHash = async(s: string, hash: string): Promise<boolean> =>{
+    return bcrypt.compare(s, hash)
+}
